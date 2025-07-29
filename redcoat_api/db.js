514a0c9219a44e6194b8b4caf88d266b4374f9ac
Ethan Villa <villaethan@Ethans-MacBook-Air.local>
2025-07-29 00:40:24 -0400

    update to api to make it production ready

--- redcoat_api/db.js
+++ redcoat_api/db.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USER,     
-    host: process.env.DB_HOST,         
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE, 
-    port: process.env.DB_PORT, 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool
