40cbfcd5ce61aa1c0e84ee0243d3dcc025b93a6d
Ethan Villa <villaethan@Ethans-MacBook-Air.local>
2025-08-12 17:06:33 -0400

    updating cors to work around the cors policy

--- redcoat_api/server.js
+++ redcoat_api/server.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(express.json());
