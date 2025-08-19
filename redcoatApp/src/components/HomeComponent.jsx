ad7031ebcfdbeab0b06a03cf5dbc56ce8d103050
Ethan Villa <villaethan@Ethans-MacBook-Air.local>
2025-08-19 15:36:06 -0400

    update home

--- redcoatApp/src/components/HomeComponent.jsx
+++ redcoatApp/src/components/HomeComponent.jsx
@@ -131,7 +131,7 @@ function Home() {
 
       {/* New Releases Section */}
       <section className="py-16 px-6 md:px-20 bg-gray-900">
-       {/* <div className="max-w-6xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <h2 className="text-4xl font-bold text-center mb-8 font-brand">New Releases</h2>
           {Array.isArray(clothingItems) && clothingItems.length > 0 && (
             <Slider {...sliderSettings}>
@@ -142,7 +142,7 @@ function Home() {
               ))}
             </Slider>
           )}
-        </div> */}
+        </div> 
 
         {/* Placeholder for clothing until it is ready */}
         <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p -10 text-center">
